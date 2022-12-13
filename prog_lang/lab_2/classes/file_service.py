980b0b79ddaf7b332ba45bf5f5c95d5d8eac79b1
Artem Shevelev <Artem1990shevelev@gmail.com>
2022-12-13 17:42:00 +0300

    final
    Исправил названия
    Теперь есть проверка в выдаче нескольких файлов
    recover(бывш. reincarnate) теперь перезаписывает max_id и айди не перезатираются

--- prog_lang/lab_2/classes/file_service.py
+++ prog_lang/lab_2/classes/file_service.py
@@ -35,16 +35,16 @@ class FileService:
         try:
             Filename = self.data.pop(id)
             self.used_ids.append(id)
-            return print(f"File '{Filename}' delete success")
+            return f"File '{Filename}' delete success"
         except:
-            return(print("FILE NOT FOUND"))
+            return "FILE NOT FOUND"
 
     # смена id
-    def change_dict(self,id, new_id):
+    def change_id(self,id, new_id):
         try:
             # проверка на уже существующий id
             if new_id in self.data:
-                return(print("Id exist already"))
+                return "Id exist already"
             #если макс айдишник меньше нового айди числа, то смотрим все возможные свободные индексы до нашего нового макс id и если их не в словаре и массиве, добавляем
             elif self.max_id <= new_id:
                 self.max_id = new_id+1
@@ -52,17 +52,17 @@ class FileService:
                 for possible_id in range (new_id):
                     if (possible_id not in self.data) and (possible_id not in self.used_ids):
                         self.used_ids.append(possible_id)
-                return(print(f"Id change success to {new_id}"))
+                return f"Id change success to {new_id}"
             # Добавляем старый айди числа, если новый меньше настоящего максимального айди
             elif self.max_id >= new_id:
                 self.data[new_id] = self.data.pop(id)
                 self.used_ids.remove(new_id)
                 if id not in self.used_ids:
                     self.used_ids.append(id)
-                return(print(f"id change success to {new_id}"))
+                return f"id change success to {new_id}"
             else:
                 self.data[new_id] = self.data.pop(id)
-                return print(f"id change success to {new_id}")
+                return f"id change success to {new_id}"
         except:
             raise "SMTH WRONG"
 
@@ -71,34 +71,39 @@ class FileService:
     def get_few_files(self, array_of_id):
         array_of_id=list(map(int,array_of_id .strip().split())) # убираются пробелы, делится на символы, мапается, создается массив
         new_arr = []
+        print(array_of_id)
         for one_id in array_of_id:
-            new_arr.append(self.data[one_id])
-        print("Ваши файлы: ",new_arr)
+            if one_id in self.data:
+                new_arr.append(self.data[one_id])
+            else:
+                print(f"file with {one_id} doesnt exist")
+        return f"Ваши файлы: {new_arr}"
 
     #резервный файл
     def backup(self):
         data_str = ""
         for i in self.data:
-            data_str += str(i) + ":" +  str(self.data[i]) + ","  # записываем в строку данные нашего словаря
-        data_str = data_str[:len(data_str)-1]  # удаляем последнюю запятую
-        handler = open("backup.txt", "w") # записываем в файл
+            data_str += str(i) + ":" +  str(self.data[i]) + ","
+        data_str = data_str[:len(data_str)-1]
+        handler = open('backup.txt', 'w')
         handler.write(data_str)
         handler.close()
+        print(self.max_id)
         return "you backup"
     # восстановление из резервного файла
-    def reincarnate(self):
-        handler = open("backup.txt", "r")
+    def recover(self):
+        handler = open('backup.txt', 'r')
         text = handler.read()
         handler.close()
-        data = dict(
-            (int(a.strip()), b.strip())
-            for a, b in(element.split(":")
-                for element in text.split(",")))        # восстанавливаем из файла по клбчам и их значениям
+        data = dict((int(a.strip()), b.strip())
+            for a, b in(element.split(':')
+                for element in text.split(',')))
         self.data = data
+        self.max_id= max(max(data.keys()),0)+1
+        self.used_ids = []
+        print(self.max_id)
         return "you recover"
     # показать все файлы
     def view_data(self):
         for id in self.data:
             print(f"{id} : {self.data[id]}")
-
-
--- prog_lang/lab_2/main.py
+++ prog_lang/lab_2/main.py
@@ -3,7 +3,7 @@ new_file_system = FileService()
 
 
 while True:
-    command = input("set - сохранить файл по ID\n get - выдача сохраненного файла\n del - удаление по ID\n change - изменение ID сохраненного файла\n few - получение нескольких файлов по ID\n backup - бэкап в файл\n reincarnate - восстановление из файла\n view - посмотреть файлы \n exit - выход:\n ")
+    command = input("set - сохранить файл по ID\n get - выдача сохраненного файла\n del - удаление по ID\n change - изменение ID сохраненного файла\n few - получение нескольких файлов по ID\n backup - бэкап в файл\n recover - восстановление из файла\n view - посмотреть файлы \n exit - выход:\n ")
     if command == "exit":
         print("Вы вышли")
         exit(0)
@@ -14,13 +14,13 @@ while True:
     elif command == "del":
         print(new_file_system.del_file(int(input("Укажите ID файла:\n"))))
     elif command == "change":
-        print(new_file_system.change_dict(int(input("Укажите id файла:")),int(input("Укажите новый id файла:"))))
+        print(new_file_system.change_id(int(input("Укажите id файла:")),int(input("Укажите новый id файла:"))))
     elif command == "few":
         print(new_file_system.get_few_files(input("Укажите ID файлов через пробел:\n")))
     elif command == "backup":
         print(new_file_system.backup())
-    elif command == "reincarnate":
-        print(new_file_system.reincarnate())
+    elif command == "recover":
+        print(new_file_system.recover())
     elif command == "view":
         print("Your files: ")
         new_file_system.view_data()
